626fdd215f5c8175de2892c52ff7ec5a26290b04
Andrey Gorlov <andreygorlov19995@gmail.com>
2024-01-19 23:49:31 +0300

    del corsOptions

--- api/index.ts
+++ api/index.ts
@@ -9,11 +9,8 @@ export default class Server {
     }
 
     private config(app: Application): void {
-        const corsOptions: CorsOptions = {
-            origin: "http://localhost:8081"
-        };
 
-        app.use(cors(corsOptions));
+        app.use(cors());
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
     }
